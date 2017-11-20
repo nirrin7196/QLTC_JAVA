3c774518ffe247ea68ed751bc266412f48417546
hoaithuong.uit <hoaithuong.uit@gmail.com>
2017-11-21 00:22:43 +0700

    fix-copy-database-in-first-activity

--- app/src/main/java/com/example/thuongdh/qltc/MainActivity.java
+++ app/src/main/java/com/example/thuongdh/qltc/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.thuongdh.qltc;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Locale;
 
 import android.content.Intent;
@@ -16,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView im;
     String lang;
-    SQLiteDatabase database;
+    SQLiteDatabase database = null;
+    public String Database_name = "QuanLyThuChiDb.sqlite";
+    public static final String DB_path = "/databases/";
     String DB_name = "QuanLyThuChiDb.sqlite";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         im = (ImageView) findViewById(R.id.imvWelcome);
         im.setImageResource(R.drawable.bg_app);
+        processCopy();
         database = openOrCreateDatabase(DB_name, MODE_PRIVATE, null);
         lang = "English";
         Cursor c = database.query("SettingTb",null,null,null,null,null,null);
@@ -51,6 +59,50 @@ public class MainActivity extends AppCompatActivity {
         }
         changeScreen();
     }
+
+    public void processCopy(){
+        File dbFile = getDatabasePath(Database_name);
+        if (!dbFile.exists()){
+            try {
+                CopyDataFromAsset();
+            } catch (Exception ex)
+            {
+
+            }
+        }
+    }
+
+    private void CopyDataFromAsset() {
+        try {
+            InputStream myinput;
+            myinput = getAssets().open(Database_name);
+            String outfilename = getDatabasePath();
+            File f = new File(getApplicationInfo().dataDir + DB_path);
+            if (!f.exists()){
+                f.mkdir();
+            }
+            OutputStream myoutput = new FileOutputStream(outfilename);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = myinput.read(buffer)) >0)
+            {
+                myoutput.write(buffer,0,length);
+            }
+            myoutput.flush();
+            myinput.close();
+            myoutput.close();
+        }catch (IOException e){
+
+        }
+    }
+
+    private String getDatabasePath() {
+        return getApplicationInfo().dataDir + DB_path + Database_name;
+    }
+
+
+
+
     public void changeScreen()
     {
         Thread start = new Thread(){
--- app/src/main/java/com/example/thuongdh/qltc/StartActivity.java
+++ app/src/main/java/com/example/thuongdh/qltc/StartActivity.java
@@ -1,15 +1,8 @@
 package com.example.thuongdh.qltc;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -26,7 +19,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        processCopy();
+
 
         btn = (ImageButton) findViewById(R.id.btnstart);
         btn.setImageResource(R.drawable.icon);
@@ -58,44 +51,5 @@ public class StartActivity extends AppCompatActivity {
 
 
 
-    SQLiteDatabase database = null;
-    public void processCopy(){
-        File dbFile = getDatabasePath(Database_name);
-        if (!dbFile.exists()){
-            try {
-                CopyDataFromAsset();
-            } catch (Exception ex)
-            {
 
-            }
-        }
-    }
-
-    private void CopyDataFromAsset() {
-        try {
-            InputStream myinput;
-            myinput = getAssets().open(Database_name);
-            String outfilename = getDatabasePath();
-            File f = new File(getApplicationInfo().dataDir + DB_path);
-            if (!f.exists()){
-                f.mkdir();
-            }
-            OutputStream myoutput = new FileOutputStream(outfilename);
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = myinput.read(buffer)) >0)
-            {
-                myoutput.write(buffer,0,length);
-            }
-            myoutput.flush();
-            myinput.close();
-            myoutput.close();
-        }catch (IOException e){
-
-        }
-    }
-
-    private String getDatabasePath() {
-        return getApplicationInfo().dataDir + DB_path + Database_name;
-    }
 }
